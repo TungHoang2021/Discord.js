268df58d4792574d88981e11f7ba46127ed13a97
TungHoang <79042425+tunghoangvn@users.noreply.github.com>
2021-09-14 17:15:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+  console.log('This Bot Is Online');
+});
+
+bot.login('ODUzMTAxNzUyODI0MTAyOTMy.YMQe6w.pJ2XIvtFGBG4Bes8lPxIbwRLkLg');
